e0752133dd49ef889c9e7eb90d4087a119bef1a6
sidcleyss <51009355+sidcleyss@users.noreply.github.com>
2020-12-02 23:53:31 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ function Home() {
             <h1>Home</h1>
             <Contador />
             <div>Teste</div>
+            <script data-ad-client="ca-pub-9328493142597493" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         </div>
     )
     
